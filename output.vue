e9f45d092f9c3c20e3d27feda1ffa623af5d2639
ccjx <icourt1@ccjxdeMacBook-Pro.local>
2020-04-20 10:48:39 +0800

    modifu json tree

--- output.vue
+++ output.vue
@@ -1,60 +1,45 @@
+<template>
+  <div class="container">
+    <div class="left_cover"></div>
 
-      
-    <template>
-    <div class="container" >
-      
-    <div class="left_cover" >
-      
-    </div>
-  
-    <div class="mid" >
-      
-    </div>
-  
-    <div class="right_cover" >
-      
-    </div>
-  
-    </div>
-  </template>
-  
-      <style>
-        
-      .container{
-        
+    <div class="mid"></div>
+
+    <div class="right_cover"></div>
+  </div>
+</template>
+
+<style>
+  .container {
     width: 396.3px;
-    height: 276px;background: #7c0505;position: relative;
-      }
-    
+    height: 276px;
+    background: #7c0505;
+    position: relative;
+  }
 
-      .left_cover{
-        
+  .left_cover {
     width: 178px;
     height: 256px;
     position: absolute;
     left: 10px;
-    top: 10px;background: green;
-      }
-    
+    top: 10px;
+    background: green;
+  }
 
-      .mid{
-        
+  .mid {
     width: 20px;
     height: 256px;
     position: absolute;
     left: 187.9px;
-    top: 10px;background: purple;
-      }
-    
+    top: 10px;
+    background: purple;
+  }
 
-      .right_cover{
-        
+  .right_cover {
     width: 178px;
     height: 256px;
     position: absolute;
     left: 208.3px;
-    top: 10px;background: gold;
-      }
-    
-      </style>
-    
+    top: 10px;
+    background: gold;
+  }
+</style>
