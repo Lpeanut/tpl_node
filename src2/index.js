21fa11d3bea7737f89b7de5dec049c6d28a001e1
ccjx <icourt1@ccjxdeMacBook-Pro.local>
2020-05-14 01:57:06 +0800

    新的结构

---
+++ src2/index.js
@@ -0,0 +1,20 @@
+const template = `
+  <container>
+    <fm>
+      <img z=0 src="" whxy=[10, 20, 50, 60] font=[40px, 40px, 500]/>
+      <img src="" wh=[10] xy=[20] />
+      <title />
+      <author />
+      <logo />
+    </fm>
+    <mid>
+      <mid-title />
+    </mid>
+    <fd>
+      <fd-svg />
+      <fd-bg />
+      <barcode />
+      <qrcode />
+    </fd>
+  </container>
+`
